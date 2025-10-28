431ede9b04b3485135deea86a2fd4499a7fac42d
Nicola Belluti <me@nicolabelluti.me>
2025-10-28 20:34:02 +0100

    Added Nix Flake for Hugo

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "A simple development workspace for working with Hugo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell = pkgs.mkShell {
+        # Packages to install
+        buildInputs = [
+          pkgs.hugo
+        ];
+      };
+    });
+}
